20be82d0eaff2fca941251fcdce712dfbd9b0f09
NICK VALLEE <nickvallee@gmail.com>
2017-01-17 13:25:58 -0500

    added all content, CSS styling mostly done

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span id="role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -38,7 +38,7 @@ var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDates = '<div class="project__date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
@@ -86,7 +86,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x, y);
 });
 
 
@@ -234,16 +238,15 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,203 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+//// BIO
+var bio = {
+    "name": "Nick Vallee",
+    "role": "Front End Developer",
+    "contacts": {
+        "mobile": "514-402-5451",
+        "email": "nickvallee@gmail.com",
+        "github": "github.com/nickvallee",
+        "twitter": "twitter.com/NickVallee",
+        "location": "Montreal"
+    },
+    "welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    "skills": [
+        "HTML", "CSS", "JAVASCRIPT", "GitHub", "Node.js", "Swift"
+    ],
+    "bioPic": "images/udacity-profile-pic.jpg",
+    "display": function() {
+        var name = HTMLheaderName.replace("%data%", bio.name);
+        var role = HTMLheaderRole.replace("%data%", bio.role);
+
+        $("#header").prepend(name, role);
+
+        var mobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var email = HTMLemail.replace("%data%", bio.contacts.email);
+        var twitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+        var github = HTMLgithub.replace("%data%", bio.contacts.github);
+        var location = HTMLlocation.replace("%data%", bio.contacts.location);
+
+        $("#topContacts").prepend(mobile, email, twitter, github, location);
+
+        var welcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+        var bioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+
+        $("#header").append(bioPic, welcomeMessage);
+
+        if (bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+
+            for (skill in bio.skills) {
+                var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+                $("#header").append(formattedSkill);
+            }
+
+        }
+
+        $("#footerContacts").append(mobile, email, twitter, github);
+
+    }
+}
+
+///EDUCATON
+
+var education = {
+    "schools": [{
+        "name": "Concordia University",
+        "dates": "2007-2010",
+        "location": "Montreal",
+        "url": "https://www.concordia.ca/",
+        "Majors": ["Political Science", "History"]
+    }],
+    "onlineCourses": [{
+        "title": "Front-End Web Developer Nanodegree",
+        "school": "Udacity",
+        "dates": "2017",
+        "url": "https://www.udacity.com/"
+    }, {
+        "title": "Git A Web Developer Job",
+        "school": "Udemy",
+        "dates": "2016",
+        "url": "https://www.udemy.com"
+    }, {
+        "title": "Front-End Devlopment Certificate",
+        "school": "Free Code Camp",
+        "dates": "2016-2017",
+        "url": "https://www.freecodecamp.com"
+    }],
+
+    "display": function() {
+
+        $("#education").append(HTMLschoolStart);
+
+        for (school in education.schools) {
+            var name = HTMLschoolName.replace("%data%", education.schools[school].name);
+            var dates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+            var location = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+            var majors = HTMLschoolMajor.replace("%data%", education.schools[school].Majors);
+
+            console.log(name, dates, location);
+
+            $("#education").append(name, dates, location, majors);
+        }
+
+        $("#education").append(HTMLonlineClasses);
+
+        for (course in education.onlineCourses) {
+            var title = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+            var school = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+            var dates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+            var url = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+
+            $("#education").append(title, school, dates, url);
+        }
+
+    }
+}
+
+/////WORK
+
+var work = {
+    "jobs": [{
+            "employer": "Folly and the Hunter",
+            "title": "Band Manager",
+            "location": "Montreal",
+            "dates": "2010-2017",
+            "description": "Responsibilities include managing money, applying for grants, maintaining social media presence, updating our website and managing tour logistics. Communicating constantly online with industry professionals, including publishers and licensers."
+        }, {
+            "employer": "Apple",
+            "title": "Specialist",
+            "location": "Montreal",
+            "dates": "2011-2014",
+            "description": "Guided customers in finding the best solution for their particular needs regarding Apple products, which included a significant amount of troubleshooting and tech support. Helped determine whether customers required a Genius Bar appointment or not."
+        }, {
+            "employer": "Idea",
+            "title": "Tech Support",
+            "location": "Montreal",
+            "dates": "2011",
+            "description": "Provided tech support for pharmaceutical representatives on MERC’s VPN, and the software and hardware associated with it. Gained experience in closing tickets, using supporting documentation to find solutions, and helping professionals with time sensitive problems."
+        }
+
+    ],
+    "display": function() {
+        for (job in work.jobs) {
+            $("#workExperience").append(HTMLworkStart);
+
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+            var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+            var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+            var formattedWorkDate = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+            var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+            $(".work-entry:last").append(formattedEmployerTitle, formattedWorkDate, formattedWorkLocation, formattedWorkDescription);
+        }
+    }
+}
+
+///PROJECTs
+
+var projects = {
+    "projects": [{
+        "title": "Portfolio Page",
+        "dates": 2016,
+        "description": "it is my website porfolio, behold",
+        "images": ["http://placehold.it/400x200"]
+    }, {
+        "title": "weather app",
+        "dates": 2016,
+        "description": "you can see the weather with this",
+        "images": ["http://placehold.it/400x200"]
+
+    }, {
+        "title": "some other project",
+        "dates": 2016,
+        "description": "this project will be amazing",
+        "images": ["http://placehold.it/400x200"]
+    }],
+    "display": function() {
+
+        for (project in projects.projects) {
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+            $(".project-entry:last").append(formattedTitle);
+
+            var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+            $(".project-entry:last").append(formattedDates);
+
+            var formattedWorkDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+            $(".project-entry:last").append(formattedWorkDescription);
+
+            if (projects.projects[project].images.length > 0) {
+                for (image in projects.projects[project].images) {
+                    var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+
+                    $(".project-entry:last").append(formattedImage);
+                }
+            }
+        }
+    }
+}
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
+
+//INSTANCES OF DISPLAY
+
+bio.display();
+work.display();
+projects.display();
+education.display();
